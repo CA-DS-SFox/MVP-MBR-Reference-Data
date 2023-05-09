5b5c19946fefb3089e08f6c74dcf2b01cd99c222
CA-DS-SFox <suzanne.fox@citizensadvice.org.uk>
2023-05-09 10:50:15 +0100

    INITIAL : First commit or script

---
+++ R_MBR_ReferenceData.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(googlesheets4)
+
+# -------------------------------------------------------------------------
+
+# get current data
+in_wbook <- 'https://docs.google.com/spreadsheets/d/1SIFJPCA8duV7qFnq6u1eq8o8tHEXMMFGCVa4Ea6nCWc/edit#gid=482107966'
+in_sheet <- 'Member List (1)'
+
+df_in <- googlesheets4::read_sheet(in_wbook, sheet = in_sheet)
+
+# -------------------------------------------------------------------------
+
+# reformat as required
+df_out <- df_in %>% 
+  mutate(member_date_start = as.character(Start_Date__c)) %>% 
+  mutate(member_date_end = as.character(End_Date__c)) %>% 
+  select(member_id = Membership_Number__c,
+         member_name = Name,
+         member_short = Short_Name__c,
+         member_date_start,
+         member_date_end) %>% 
+  mutate(member_aws = str_replace(member_id, '/',''), .after='member_id') %>% 
+  mutate(upload_date = Sys.Date()) %>% 
+  identity()
+
+# -------------------------------------------------------------------------
+
+out_wbook <- 'https://docs.google.com/spreadsheets/d/1FEcmgQmYk_Dmf9IgjM1CdYjSFTMQzHB2DyiMpzpbCas/edit#gid=405822670'
+out_sheet <- 'reference_mbr'
+
+googlesheets4::write_sheet(df_out, out_wbook, out_sheet)
